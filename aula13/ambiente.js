bba54551757999351ff8f319e5478f9786764371
johnnyzudo <116656198+johnnyzudo@users.noreply.github.com>
2022-12-08 14:02:44 -0300

    criei o ex015 da aula012 e criei os exercícios da aula013

---
+++ aula13/ambiente.js
@@ -0,0 +1,19 @@
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 3)
+
+/*
+var c = 1
+while (c <= 3) {
+    console.log(`Passo ${c}`)
+    c++
+}
+*/
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
